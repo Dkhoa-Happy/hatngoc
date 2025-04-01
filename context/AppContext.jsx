9c7dff428bddfd3bcfa9322d9ccfc7e82431097c
Dang Khoa <ojtgrowth@sendo.vn>
2025-04-01 15:10:47 +0700

    Update environment variables, add axios dependency, implement user data fetching, and enhance product display with VND formatting

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -2,7 +2,9 @@
 import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -15,6 +17,7 @@ export const AppContextProvider = (props) => {
   const router = useRouter();
 
   const { user } = useUser();
+  const { getToken } = useAuth();
 
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
@@ -30,9 +33,21 @@ export const AppContextProvider = (props) => {
       if (user.publicMetadata.role === "seller") {
         setIsSeller(true);
       }
-      setUserData(userDummyData);
+
+      const token = await getToken();
+
+      const { data } = await axios.get("/api/user/data", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserData(data.user);
+        setCartItems(data.user.cartItems);
+      } else {
+        toast.error(data.message);
+      }
     } catch (e) {
-      throw e;
+      toast.error(error.message);
     }
   };
 
@@ -89,6 +104,7 @@ export const AppContextProvider = (props) => {
 
   const value = {
     user,
+    getToken,
     currency,
     router,
     isSeller,
